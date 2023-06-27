7e1d5be7696ef2fff04925363672da1e679592ff
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2023-06-27 15:34:31 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from rembg import remove, new_session
+
+session = new_session()
+
+for file in Path('input').glob('*.jpg'):
+    input_path = str(file)
+    output_path = str(file.parent / (file.stem + ".out.png"))
+
+    with open(input_path, 'rb') as i:
+        with open(output_path, 'wb') as o:
+            input = i.read()
+            output = remove(input, session=session)
+            o.write(output)
