23ff06cea98e03ed66a5ec2643778baaa2762416
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2023-06-27 15:50:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ for file in Path('input').glob('*.jpg'):
     with open(input_path, 'rb') as i:
         with open(output_path, 'wb') as o:
             input = i.read()
-            print("converting ", input)
+            print("Removing background from image: ", input_path)
             output = remove(input, session=session)
             o.write(output)
